71f340b55b04b680f36435a2a83db2d46d81d8b2
Pablo Carrera <pabloce@users.noreply.huggingface.co>
2024-05-25 04:53:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,10 @@ PLACEHOLDER = """
         <p style="text-align: left; font-size: 16px; line-height: 1.5; margin-bottom: 15px;">The llama-cpp-agent framework you can engage in seamless conversations with most `open source LLMs`, even small ones. Execute functions using LLMs, single and parallel function calling. Process text using agent chains, with tools.</p>
         <div style="display: flex; justify-content: space-between; align-items: center;">
             <div style="display: flex; flex-flow: column; justify-content: space-between;">
-                <span style="display: inline-flex; align-items: center; border-radius: 0.375rem; background-color: rgba(229, 70, 77, 0.1); padding: 0.1rem 0.75rem; font-size: 0.75rem; font-weight: 500; color: #f88181; margin-top: 2.5px;">
+                <span style="display: inline-flex; align-items: center; border-radius: 0.375rem; background-color: rgba(229, 70, 77, 0.1); padding: 0.1rem 0.75rem; font-size: 0.75rem; font-weight: 500; color: #f88181; margin-bottom: 2.5px;">
                     Mistral 7B Instruct v0.3
                 </span>
-                <span style="display: inline-flex; align-items: center; border-radius: 0.375rem; background-color: rgba(79, 70, 229, 0.1); padding: 0.1rem 0.75rem; font-size: 0.75rem; font-weight: 500; color: #60a5fa; margin-bottom: 2.5px;">
+                <span style="display: inline-flex; align-items: center; border-radius: 0.375rem; background-color: rgba(79, 70, 229, 0.1); padding: 0.1rem 0.75rem; font-size: 0.75rem; font-weight: 500; color: #60a5fa; margin-top: 2.5px;">
                     Meta Llama 3 8B Instruct
                 </span>
             </div>

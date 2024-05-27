2030d7291a761fe4ace570968e839c28bf666903
Pablo Carrera <pabloce@users.noreply.huggingface.co>
2024-05-27 22:28:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ hf_hub_download(
     filename="Mistral-7B-Instruct-v0.3-Q6_K.gguf",
     local_dir="./models"
 )
-# hf_hub_download(
-#     repo_id="bartowski/Meta-Llama-3-8B-Instruct-GGUF",
-#     filename="Meta-Llama-3-8B-Instruct-Q6_K.gguf",
-#     local_dir="./models"
-# )
+hf_hub_download(
+    repo_id="bartowski/Meta-Llama-3-8B-Instruct-GGUF",
+    filename="Meta-Llama-3-8B-Instruct-Q6_K.gguf",
+    local_dir="./models"
+)
 
 css = """
 .message-row {
